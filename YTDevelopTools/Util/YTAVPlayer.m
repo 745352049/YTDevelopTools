43ca39cd62f8500d5619da5d3f4ad3e51532faa2
iMac <745352049@qq.com>
2020-07-21 08:49:02 +0800

    Update YTAVPlayer

--- YTDevelopTools/Util/YTAVPlayer.m
+++ YTDevelopTools/Util/YTAVPlayer.m
@@ -7,6 +7,7 @@
 @interface YTAVPlayer ()
 
 @property (nonatomic, strong) id timeObserver;
+@property (nonatomic, strong, readwrite) AVPlayer *player;
 
 @end
 
@@ -17,64 +18,43 @@
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         player = [[YTAVPlayer alloc] init];
-        player.interval = CMTimeMake(1.0, 100.0);
+        player.timeObserverInterval = CMTimeMake(1.0, 100.0);
     });
     return player;
 }
 
-- (void)playNetAudioWithPath:(NSString *)path {
-    [self removeTimeObserver];
-    [self removePlayStatus];
-    [self.audioSession setCategory:AVAudioSessionCategoryAmbient error:nil];
-    
-    NSURL *url = [NSURL fileURLWithPath:path];
-    if ([path hasPrefix:@"http://"] || [path hasPrefix:@"https://"]) {
-        url = [NSURL URLWithString:[path stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
++ (BOOL)accessInstanceVariablesDirectly {
+    return NO;
+}
+
+- (void)pause {
+    if (self.isPlaying == YES) {
+        [self.player pause];
+        [self playerState:YTPlayerStateType_Pause];
     }
-    
-    AVPlayerItem *playerItem = [[AVPlayerItem alloc] initWithURL:[NSURL URLWithString:[path stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]]];
-    self.player = [[AVPlayer alloc] initWithPlayerItem:playerItem];
-    [self.player.currentItem addObserver:self forKeyPath:@"status" options:NSKeyValueObservingOptionNew context:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(playerDidPlayToEnd:) name:AVPlayerItemDidPlayToEndTimeNotification object:playerItem];
-    __weak typeof(self) weakSelf = self;
-    self.timeObserver = [self.player addPeriodicTimeObserverForInterval:self.interval queue:dispatch_queue_create("com.player.progress", DISPATCH_QUEUE_CONCURRENT) usingBlock:^(CMTime time) {
-        // 当前播放的时间
-        float current = CMTimeGetSeconds(time);
-        // 总时间
-        float total = CMTimeGetSeconds(playerItem.duration);
-        if (weakSelf.playProgressBlock) {
-            weakSelf.playProgressBlock(current, total, weakSelf.player);
-        }
-    }];
 }
 
-- (void)playLocalAudioWithPath:(NSString *)path {
-    [self removeTimeObserver];
-    [self removePlayStatus];
-    [self.audioSession setCategory:AVAudioSessionCategoryAmbient error:nil];
-    
-    AVPlayerItem *playerItem = [[AVPlayerItem alloc] initWithURL:[NSURL fileURLWithPath:path]];
-    self.player = [[AVPlayer alloc] initWithPlayerItem:playerItem];
-    [self.player.currentItem addObserver:self forKeyPath:@"status" options:NSKeyValueObservingOptionNew context:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(playerDidPlayToEnd:) name:AVPlayerItemDidPlayToEndTimeNotification object:playerItem];
-    __weak typeof(self) weakSelf = self;
-    self.timeObserver = [self.player addPeriodicTimeObserverForInterval:self.interval queue:dispatch_queue_create("com.player.progress", DISPATCH_QUEUE_CONCURRENT) usingBlock:^(CMTime time) {
-        // 当前播放的时间
-        float current = CMTimeGetSeconds(time);
-        // 总时间
-        float total = CMTimeGetSeconds(playerItem.duration);
-        if (weakSelf.playProgressBlock) {
-            weakSelf.playProgressBlock(current, total, weakSelf.player);
-        }
-    }];
+- (void)play {
+    if (self.isPlaying == NO) {
+        [self.player play];
+        [self playerState:YTPlayerStateType_Start];
+    }
 }
 
-- (void)playerDidPlayToEnd:(NSNotification *)notification {
-    [self stopNoBlock];
-    
+- (void)playerState:(YTPlayerStateType)state {
+    [[NSNotificationCenter defaultCenter] postNotificationName:YTAVPlayerStateNotification object:nil userInfo:@{YTAVPlayerState : [NSString stringWithFormat:@"%lu", (unsigned long)state], YTAVPlayerPath : self.playMusicPath}];
     if (self.playerStateBlock) {
-        self.playerStateBlock(self.player, PlayerStateType_Stop);
+        self.playerStateBlock(self.player, self.playerItem, state);
     }
+    if (state == YTPlayerStateType_Start) {
+        _isPlaying = YES;
+    } else {
+        _isPlaying = NO;
+    }
+}
+
+- (void)playerDidPlayEnd:(NSNotification *)notification {
+    [self playerState:YTPlayerStateType_End];
 }
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
@@ -82,98 +62,93 @@
         switch (self.player.status) {
             case AVPlayerItemStatusUnknown:
             {
-                [self stopNoBlock];
-                if (self.playerStateBlock) {
-                    self.playerStateBlock(self.player, PlayerStateType_Unknown);
-                }
+                [self playerState:YTPlayerStateType_Unknown];
             }
                 break;
             case AVPlayerItemStatusReadyToPlay:
             {
                 [self.player play];
-                if (self.playerStateBlock) {
-                    self.playerStateBlock(self.player, PlayerStateType_Start);
-                }
+                [self playerState:YTPlayerStateType_Start];
             }
                 break;
             case AVPlayerItemStatusFailed:
             {
-                [self stopNoBlock];
-                if (self.playerStateBlock) {
-                    self.playerStateBlock(self.player, PlayerStateType_Failed);
-                }
+                [self playerState:YTPlayerStateType_Failed];
             }
                 break;
             default:
                 break;
         }
-    }
-}
-
-- (void)pause {
-    if (_player && _player.rate == 1.0) {
-        [self.player pause];
-        if (self.playerStateBlock) {
-            self.playerStateBlock(self.player, PlayerStateType_Pause);
+    } else if ([keyPath isEqualToString:@"loadedTimeRanges"]) {
+        NSArray *array = self.player.currentItem.loadedTimeRanges;
+        /// 本次缓冲的时间范围
+        CMTimeRange timeRange = [array.firstObject CMTimeRangeValue];
+        /// 缓冲总长度
+        NSTimeInterval totalBuffer = CMTimeGetSeconds(timeRange.start) + CMTimeGetSeconds(timeRange.duration);
+        /// 音乐的总时间
+        NSTimeInterval duration = CMTimeGetSeconds(self.player.currentItem.duration);
+        if (self.playerBufferBlock) {
+            self.playerBufferBlock(self.player, self.playerItem, duration, totalBuffer);
         }
     }
 }
 
-- (void)play {
-    if (_player && _player.rate == 0.0) {
-        [self.player play];
-        if (self.playerStateBlock) {
-            self.playerStateBlock(self.player, PlayerStateType_Start);
-        }
+- (void)removePlayerItemObserver {
+    [self.player.currentItem removeObserver:self forKeyPath:@"status"];
+    [self.player.currentItem removeObserver:self forKeyPath:@"loadedTimeRanges"];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:AVPlayerItemDidPlayToEndTimeNotification object:self.player.currentItem];
+}
+
+- (void)addPlayerItemObserver {
+    [self.playerItem addObserver:self forKeyPath:@"status" options:NSKeyValueObservingOptionNew context:nil];
+    [self.playerItem addObserver:self forKeyPath:@"loadedTimeRanges" options:NSKeyValueObservingOptionNew context:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(playerDidPlayEnd:) name:AVPlayerItemDidPlayToEndTimeNotification object:self.playerItem];
+}
+
+- (void)setPlayMusicPath:(NSString *)playMusicPath {
+    _playMusicPath = playMusicPath;
+    
+    if ([playMusicPath hasPrefix:@"http://"] || [playMusicPath hasPrefix:@"https://"]) {
+        self.playerItem = [[AVPlayerItem alloc] initWithURL:[NSURL URLWithString:[playMusicPath stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]]];
+    } else {
+        self.playerItem = [[AVPlayerItem alloc] initWithURL:[NSURL fileURLWithPath:playMusicPath]];
+    }
+    if (self.player.currentItem != self.playerItem) {
+        [self removePlayerItemObserver];
+        [self.player replaceCurrentItemWithPlayerItem:self.playerItem];
+        [self addPlayerItemObserver];
     }
 }
 
-- (void)stop {
-    if (_player) {
-        [self removeTimeObserver];
-        [self removePlayStatus];
-        [self.player pause];
-        [[NSNotificationCenter defaultCenter] removeObserver:self];
-        if (self.playerStateBlock) {
-            self.playerStateBlock(self.player, PlayerStateType_Stop);
-        }
-        self.player = nil;
-    }
+- (void)setTimeObserverInterval:(CMTime)timeObserverInterval {
+    _timeObserverInterval = timeObserverInterval;
 }
 
-- (void)setInterval:(CMTime)interval {
-    _interval = interval;
+- (AVPlayer *)player {
+    if (!_player) {
+        _player = [[AVPlayer alloc] initWithPlayerItem:self.playerItem];
+        [_player.currentItem addObserver:self forKeyPath:@"status" options:NSKeyValueObservingOptionNew context:nil];
+        [_player.currentItem addObserver:self forKeyPath:@"loadedTimeRanges" options:NSKeyValueObservingOptionNew context:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(playerDidPlayEnd:) name:AVPlayerItemDidPlayToEndTimeNotification object:self.playerItem];
+        __weak typeof(self) weakSelf = self;
+        self.timeObserver = [self.player addPeriodicTimeObserverForInterval:self.timeObserverInterval queue:dispatch_queue_create("com.player.progress", DISPATCH_QUEUE_CONCURRENT) usingBlock:^(CMTime time) {
+            // 当前播放的时间
+            float current = CMTimeGetSeconds(time);
+            // 总时间
+            float total = CMTimeGetSeconds(weakSelf.playerItem.duration);
+            if (weakSelf.playProgressBlock) {
+                weakSelf.playProgressBlock(weakSelf.player, weakSelf.playerItem, current, total);
+            }
+        }];
+    }
+    return _player;
 }
 
-- (void)stopNoBlock {
-    if (_player) {
-        [self removeTimeObserver];
-        [self removePlayStatus];
-        [self.player pause];
-        [[NSNotificationCenter defaultCenter] removeObserver:self];
-        self.player = nil;
+- (AVPlayerItem *)playerItem {
+    if (!_playerItem) {
+        _playerItem = [[AVPlayerItem alloc] initWithURL:[NSURL URLWithString:@""]];
     }
-}
-
-- (void)removeTimeObserver {
-    if (_timeObserver) {
-        [self.player removeTimeObserver:self.timeObserver];
-        self.timeObserver = nil;
-    }
-}
-
-- (void)removePlayStatus {
-    if (_player.currentItem != nil) {
-        [self.player.currentItem removeObserver:self forKeyPath:@"status"];
-    }
-}
-
-- (AVAudioSession *)audioSession {
-    if (!_audioSession) {
-        _audioSession = [AVAudioSession sharedInstance];
-        [_audioSession setActive:YES error:nil];
-    }
-    return _audioSession;
+    return _playerItem;
 }
 
 @end
